6fadefc82c72b8c8573e3c649ba1241ac39fca18
LethaboDiscovery <lmasetloa@yahoo.com>
2024-10-04 03:39:10 -0700

    check for Enabled features

--- ZapperTest/Program.cs
+++ ZapperTest/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ZapperTest;
+
+Console.WriteLine(UserSettings.IsFeatureEnabled("00000000", 7));//false
+Console.WriteLine(UserSettings.IsFeatureEnabled("00000010", 7));//true
+Console.WriteLine(UserSettings.IsFeatureEnabled("11111111", 9));//true
+Console.WriteLine(UserSettings.IsFeatureEnabled("00000001", 7));//false
+
---
+++ ZapperTest/UserSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZapperTest
+{
+    public  class UserSettings
+    {
+        public static bool IsFeatureEnabled(string settings, int  featureId)
+        {
+            bool IsValidFeature = (featureId >= 1 && featureId <= 8) ? true: false;
+
+            return settings[featureId - 1] == '1';
+        }
+    }
+}
